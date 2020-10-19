3f99ae03f7cc663078583a46860989e53306a8b7
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2020-10-19 13:47:22 +0200

    Add project files.

---
+++ PeerToPeerLib/FileEndPoint.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeerToPeerLib
+{
+    public class FileEndPoint
+    {
+        public Dictionary<string, int> EndPoints { get; set; }
+    }
+}
