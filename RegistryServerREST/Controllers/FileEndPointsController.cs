7ed224bb78f7c6bd84f32a8d37eab3765e85cd05
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2020-10-19 13:57:52 +0200

    Created project and added some basic stuff.

---
+++ RegistryServerREST/Controllers/FileEndPointsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RegistryServerREST.Controllers
+{
+    [ApiController]
+    [Route("api/powernap/")]
+    public class FileEndPointsController : ControllerBase
+    {
+        public string GetAllPeers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
