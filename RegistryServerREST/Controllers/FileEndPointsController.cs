19ceb7eba2e7646606b9b157e954fdff07abb2cb
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2020-10-20 10:14:31 +0200

    GetAllPeers implemented. Test constructor made in FileEndPoint model.

--- RegistryServerREST/Controllers/FileEndPointsController.cs
+++ RegistryServerREST/Controllers/FileEndPointsController.cs
@@ -1,23 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PeerToPeerLib;
 
 namespace RegistryServerREST.Controllers
 {
-    [ApiController]
     [Route("api/powernap/")]
+    [ApiController]
     public class FileEndPointsController : ControllerBase
     {
-        private List<FileEndPoint> test = new List<FileEndPoint>
+        private List<FileEndPoint> fileEndPoints = new List<FileEndPoint>
         {
-            new FileEndPoint("123.")
+            new FileEndPoint("123.3.15.312", 4646, true),
+            new FileEndPoint("123.32.51.1", 4000, true)
         };
-        public string GetAllPeers()
+
+        [HttpGet("{fileName}")]
+        public string GetAllPeers(string fileName)
         {
-            
-            
+            List<FileEndPoint> output = new List<FileEndPoint>();
+
+            foreach (var fileEndPoint in fileEndPoints)
+            {
+                if (fileEndPoint.FilesInEndPoint.ContainsKey(fileName))
+                {
+                    output.Add(fileEndPoint);
+                }
+            }
+
+            string serializedOutput = JsonSerializer.Serialize(output);
+
+            return serializedOutput;
         }
     }
 }
